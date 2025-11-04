1c1b46256763df94d1bd7ca80e4430aea0cadfbd
odukong <ohbin1017@gmail.com>
2025-11-04 21:13:08 +0900

    feat :  useGameState hooks 구현
    - 게임보드(카드)를 초기화(생성)하고 카드 클릭 이벤트 및 동일 카드 체크 이벤트 훅을 구현하였습니다.

---
+++ Week3/Homework/src/hooks/useGameState.js
@@ -0,0 +1,81 @@
+import { useCallback, useEffect, useState } from "react";
+import { buildDeck } from "../utils/buildDeck";
+
+export function useGameState(initialLevel = 1) {
+  const [deckInfo, setDeckInfo] = useState({
+    status: "idle",
+    data: null,
+    level: initialLevel,
+  }); // 생성한 카드 덱 정보 (준비상태, 카드데이터, 레벨)
+  const [currentClickCard, setCurrentClickCard] = useState([]); // (최근) 클릭된 카드 리스트
+  const [isChecking, setIsChecking] = useState(false); // 매칭되는 상태인지 체크
+  const [gameId, setGameId] = useState(1); // 게임 리셋 감지 키
+
+  // 카드 덱 생성 함수
+  const handleGenerateDeck = useCallback((level) => {
+    const data = buildDeck(level);
+    setDeckInfo({ status: "ready", data, level });
+    setCurrentClickCard([]);
+    setIsChecking(false);
+    setGameId((id) => id + 1);
+  }, []);
+
+  // 카드 덱 초기화
+  useEffect(() => {
+    handleGenerateDeck(deckInfo.level);
+  }, [deckInfo.level, handleGenerateDeck]);
+
+  // 카드 클릭 이벤트 함수
+  const handleCardClick = (clickedCard) => {
+    if (isChecking || clickedCard.isClicked || clickedCard.isMatched) return;
+
+    setDeckInfo((prev) => ({
+      ...prev,
+      data: prev.data.map((card) =>
+        card.id === clickedCard.id ? { ...card, isClicked: true } : card
+      ),
+    }));
+    setCurrentClickCard((prev) => [...prev, clickedCard]);
+  };
+
+  // 카드 값 동일 여부 체크 함수
+  useEffect(() => {
+    if (currentClickCard.length !== 2) return;
+
+    setIsChecking(true);
+
+    const [card1, card2] = currentClickCard;
+
+    if (card1.value === card2.value) {
+      setTimeout(() => {
+        setDeckInfo((prev) => ({
+          ...prev,
+          data: prev.data.map((card) =>
+            card.id === card1.id || card.id === card2.id
+              ? { ...card, isMatched: true }
+              : card
+          ),
+        }));
+
+        setCurrentClickCard([]);
+        setIsChecking(false);
+      }, 300);
+    } else {
+      setTimeout(() => {
+        setDeckInfo((prev) => ({
+          ...prev,
+          data: prev.data.map((card) =>
+            card.id === card1.id || card.id === card2.id
+              ? { ...card, isClicked: false }
+              : card
+          ),
+        }));
+
+        setCurrentClickCard([]);
+        setIsChecking(false);
+      }, 300);
+    }
+  }, [currentClickCard, deckInfo.data]);
+
+  return { deckInfo, gameId, handleGenerateDeck, handleCardClick };
+}

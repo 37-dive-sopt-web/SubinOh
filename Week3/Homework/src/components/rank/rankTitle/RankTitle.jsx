458c55fd7ef2e7d6be50cdf9b0c07ada721996c1
odukong <ohbin1017@gmail.com>
2025-11-05 13:51:15 +0900

    feat : 기록 초기화 기능 구현

--- Week3/Homework/src/components/rank/rankTitle/RankTitle.jsx
+++ Week3/Homework/src/components/rank/rankTitle/RankTitle.jsx
@@ -1,9 +1,11 @@
 import * as styles from "./RankTitle.css";
-export function RankTitle() {
+export function RankTitle({ onReset }) {
   return (
     <div className={styles.titleWrapper}>
       <h2>랭킹 보드</h2>
-      <button className={styles.button}>기록 초기화</button>
+      <button className={styles.button} onClick={onReset}>
+        기록 초기화
+      </button>
     </div>
   );
 }
--- Week3/Homework/src/pages/rank/RankPage.jsx
+++ Week3/Homework/src/pages/rank/RankPage.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { RankTable } from "../../components/rank/rankTable/RankTable";
 import { RankTitle } from "../../components/rank/rankTitle/RankTitle";
-import { getGameLogs, LOCAL_STORAGE_KEY } from "../../utils/gameStorage";
+import { getGameLogs, setGameLogs } from "../../utils/gameStorage";
 import * as styles from "./RankPage.css";
 
 export function RankPage() {
-  const gameLogs = getGameLogs();
+  const [logs, setLogs] = useState(getGameLogs());
+  const resetGameLogs = () => {
+    setGameLogs([]);
+    setLogs([]);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <RankTitle />
-      <RankTable gameLogs={gameLogs} />
+      <RankTitle onReset={resetGameLogs} />
+      <RankTable gameLogs={logs} />
     </div>
   );
 }

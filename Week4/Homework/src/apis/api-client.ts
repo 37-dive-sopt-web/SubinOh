5663d5bc5d581818034c8ad56489320bb9ec1320
odukong <ohbin1017@gmail.com>
2025-11-17 02:51:07 +0900

    feat : ky 공통 인스턴스 구현

---
+++ Week4/Homework/src/apis/api-client.ts
@@ -0,0 +1,37 @@
+import ky, { type Options, type ResponsePromise } from "ky";
+
+const http = ky.create({
+  prefixUrl: `${import.meta.env.VITE_API_KEY}`,
+  credentials: "include",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  hooks: {
+    beforeError: [
+      async (error) => {
+        const { response } = error;
+        if (response) {
+          const message = await response.text();
+          error = JSON.parse(message);
+        }
+        return error;
+      },
+    ],
+  },
+});
+
+async function parseResponse<T>(res: ResponsePromise): Promise<T> {
+  return await res.json<T>();
+}
+
+export const apiClient = {
+  get: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(http.get(pathname, options)),
+  post: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(http.post(pathname, options)),
+  patch: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(http.patch(pathname, options)),
+  delete: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(http.delete(pathname, options)),
+};
